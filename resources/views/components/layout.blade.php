68ca5634f8596e061bfcaec1aeb1a822dff5eb1b
Ricky <hahaguaha53@gmail.com>
2026-05-02 20:37:31 +0700

    Add : [Fakultas view]

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,4 +1,4 @@
-!<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,6 +7,15 @@
     @vite([])
 </head>
 <body>
-    {{$slot}}
+    <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<main>
+         {{$slot}}
 </body>
 </html>
