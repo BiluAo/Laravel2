10df6058b17c4af31a5c8f7089fad51143de6fa1
Ricky <hahaguaha53@gmail.com>
2026-05-23 20:27:08 +0700

    init: [Laravel]

--- resources/views/prodi/create.blade.php
+++ resources/views/prodi/create.blade.php
@@ -22,13 +22,16 @@
                         <form action="{{ route('prodi.store') }}" method="POST">
                             @csrf
 
+
                             <div class="mb-3">
-                                <label for="fakultas_id" class="form-label">Fakultas <span class="text-danger">*</span></label>
-                                <select name="fakultas_id" id="fakultas_id" class="form-select @error('fakultas_id') is-invalid @enderror" required>
+                                <label for="fakultas" class="form-label">
+                                    Fakultas
+                                </label>
+                                <select class="form-select" name="fakultas_id" id="fakultas">
                                     <option value="">Pilih Fakultas</option>
-                                    @foreach($fakultas as $f)
-                                        <option value="{{ $f->id }}" {{ old('fakultas_id') == $f->id ? 'selected' : '' }}>
-                                            {{ $f->nama_fakultas }}
+                                    @foreach ($listfakultas as $fakultas)
+                                        <option value="{{ $fakultas->id }}">
+                                            {{ $fakultas->nama_fakultas }}
                                         </option>
                                     @endforeach
                                 </select>
@@ -38,42 +41,34 @@
                             </div>
 
                             <div class="mb-3">
-                                <label for="alias_prodi" class="form-label">Alias Prodi <span class="text-danger">*</span></label>
-                                <input type="text" 
-                                       name="alias_prodi" 
-                                       id="alias_prodi" 
-                                       class="form-control @error('alias_prodi') is-invalid @enderror"
-                                       value="{{ old('alias_prodi') }}"
-                                       placeholder="Contoh: TI, SI, IF"
-                                       required>
+                                <label for="alias_prodi" class="form-label">Alias Prodi <span
+                                        class="text-danger">*</span></label>
+                                <input type="text" name="alias_prodi" id="alias_prodi"
+                                    class="form-control @error('alias_prodi') is-invalid @enderror"
+                                    value="{{ old('alias_prodi') }}" placeholder="Contoh: TI, SI, IF" required>
                                 @error('alias_prodi')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
 
                             <div class="mb-3">
-                                <label for="nama_prodi" class="form-label">Nama Program Studi <span class="text-danger">*</span></label>
-                                <input type="text" 
-                                       name="nama_prodi" 
-                                       id="nama_prodi" 
-                                       class="form-control @error('nama_prodi') is-invalid @enderror"
-                                       value="{{ old('nama_prodi') }}"
-                                       placeholder="Contoh: Teknik Informatika"
-                                       required>
+                                <label for="nama_prodi" class="form-label">Nama Program Studi <span
+                                        class="text-danger">*</span></label>
+                                <input type="text" name="nama_prodi" id="nama_prodi"
+                                    class="form-control @error('nama_prodi') is-invalid @enderror"
+                                    value="{{ old('nama_prodi') }}" placeholder="Contoh: Teknik Informatika" required>
                                 @error('nama_prodi')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
 
                             <div class="mb-3">
-                                <label for="nama_kaprodi" class="form-label">Ketua Program Studi <span class="text-danger">*</span></label>
-                                <input type="text" 
-                                       name="nama_kaprodi" 
-                                       id="nama_kaprodi" 
-                                       class="form-control @error('nama_kaprodi') is-invalid @enderror"
-                                       value="{{ old('nama_kaprodi') }}"
-                                       placeholder="Contoh: Dr. Ahmad Budiman, M.Kom"
-                                       required>
+                                <label for="nama_kaprodi" class="form-label">Ketua Program Studi <span
+                                        class="text-danger">*</span></label>
+                                <input type="text" name="nama_kaprodi" id="nama_kaprodi"
+                                    class="form-control @error('nama_kaprodi') is-invalid @enderror"
+                                    value="{{ old('nama_kaprodi') }}" placeholder="Contoh: Dr. Ahmad Budiman, M.Kom"
+                                    required>
                                 @error('nama_kaprodi')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
